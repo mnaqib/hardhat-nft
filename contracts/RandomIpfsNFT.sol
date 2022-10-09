310b3d440cc3d92255145d32de95636caaf50997
Mohammed Naqib <naqibdargaroad@gmail.com>
2022-10-09 20:33:10 +0530

    completed writing rin contract

--- contracts/RandomIpfsNFT.sol
+++ contracts/RandomIpfsNFT.sol
@@ -2,12 +2,136 @@
 
 pragma solidity ^0.8.7;
 
-contract RandomIpfsNFT {
-    function requestNFT() public {}
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+//custom errors.
+error RIN__RangeOutOfBounds();
+error RIN__NeedMoreETHSent();
+error RIN__TransferFailed();
+
+contract RandomIpfsNFT is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
+    //Type decl
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
+
+    //chainlink VRF variables.
+    VRFCoordinatorV2Interface private immutable i_vrfCoorinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    //VRF helpers.
+    mapping(uint256 => address) private s_requestIdToSender;
+
+    //NFT variables
+    uint256 private s_tokenCounter;
+    uint256 internal constant MAX_CHANCE_VALUE = 100;
+    string[] internal s_dogTokenUris;
+    uint256 internal i_mintFee;
+
+    //Events
+    event NFTRequested(uint256 indexed requestId, address requester);
+    event NFTMinted(Breed dogBreed, address minter);
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit,
+        string[3] memory dogTokenUris,
+        uint256 mintFee
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") {
+        i_vrfCoorinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
+        s_dogTokenUris = dogTokenUris;
+        i_mintFee = mintFee;
+    }
+
+    function requestNFT() public payable returns (uint256 requestId) {
+        if (msg.value < i_mintFee) {
+            revert RIN__NeedMoreETHSent();
+        }
+
+        requestId = i_vrfCoorinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+
+        s_requestIdToSender[requestId] = msg.sender;
+        emit NFTRequested(requestId, msg.sender);
+    }
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
         internal
-    {}
+        override
+    {
+        address nftOwner = s_requestIdToSender[requestId];
+        uint256 newTokenId = s_tokenCounter;
 
-    function tokenURI(uint256) public {}
+        uint256 moddedRNG = randomWords[0] % MAX_CHANCE_VALUE;
+        Breed dogBreed = getBreedFromModdedRNG(moddedRNG);
+        _safeMint(nftOwner, newTokenId);
+        _setTokenURI(newTokenId, s_dogTokenUris[uint256(dogBreed)]);
+        emit NFTMinted(dogBreed, nftOwner);
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+
+        if (!success) {
+            revert RIN__TransferFailed();
+        }
+    }
+
+    function getBreedFromModdedRNG(uint256 moddedRNG)
+        public
+        pure
+        returns (Breed)
+    {
+        uint256 sum = 0;
+        uint256[3] memory chanceArray = getChanceArray();
+
+        for (uint256 i = 0; i < chanceArray.length; i++) {
+            if (moddedRNG >= sum && moddedRNG < sum + chanceArray[i]) {
+                return Breed(i);
+            }
+            sum += chanceArray[i];
+        }
+
+        revert RIN__RangeOutOfBounds();
+    }
+
+    function getChanceArray() public pure returns (uint256[3] memory) {
+        return [10, 30, MAX_CHANCE_VALUE];
+    }
+
+    function getMintFee() public view returns (uint256) {
+        return i_mintFee;
+    }
+
+    function getDogTokenUris(uint256 index)
+        public
+        view
+        returns (string memory)
+    {
+        return s_dogTokenUris[index];
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }
